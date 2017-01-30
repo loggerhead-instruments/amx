d2e8e50884b9c2d8221c8ca2fb124f210dfb8e74
davidallenmann <dm@humu.io>
2017-01-30 16:18:57 -0500

    red led if SD errro

--- Arduino/amx/amx.ino
+++ Arduino/amx/amx.ino
@@ -150,7 +150,7 @@ unsigned int audioIntervalCount = 0;
 int systemGain = 4; // SG in script file
 
 int recMode = MODE_NORMAL;
-long rec_dur = 30; // seconds
+long rec_dur = 300; // seconds
 long rec_int = 0;
 int wakeahead = 10;  //wake from snooze to give hydrophone and camera time to power up
 int snooze_hour;
@@ -365,7 +365,10 @@ void setup() {
 //      display.println("SD error. Restart.");
 //      displayClock(getTeensy3Time(), BOTTOM);
 //      display.display();
-      delay(1000);
+      delay(200);
+      digitalWrite(ledRed, HIGH);
+      delay(200);
+      digitalWrite(ledRed, LOW);
     }
   }
   //SdFile::dateTimeCallback(file_date_time);
